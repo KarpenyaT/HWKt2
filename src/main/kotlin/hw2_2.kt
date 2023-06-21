803c7c812046770d8bf72ef6e1400f34d7d74cde
Tatyana Karpenya <karpenya.tatyana@mail.ru>
2023-06-21 16:42:24 +0200

    fix error main.kt

--- src/main/kotlin/hw2_2.kt
+++ src/main/kotlin/hw2_2.kt
@@ -2,10 +2,10 @@ fun main() {
     val likes = 311
     var strLikes: String
 
-    if (likes%10==1 && likes%100!=11){
-        strLikes="Понравилось $likes человеку"
-    }else{
-        strLikes="Понравилось $likes людям"
+    if (likes % 10 == 1 && likes % 100 != 11) {
+        strLikes = "Понравилось $likes человеку"
+    } else {
+        strLikes = "Понравилось $likes людям"
     }
     println(strLikes)
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,14 @@
 import kotlin.math.round
 
-fun main(){
-    val amount=35
-    val minComm=35
-    val comm:Int
-    val persComm=0.0075
-    if (amount<=minComm){
-        comm=minComm
-    }else{
-        comm= round(amount*persComm).toInt()
+fun main() {
+    val amount = 23500
+    val minComm = 35
+    val comm: Int
+    val percComm = 0.0075
+    if (amount * percComm <= minComm) {
+        comm = minComm
+    } else {
+        comm = round(amount * percComm).toInt()
     }
     println(comm)
 }
